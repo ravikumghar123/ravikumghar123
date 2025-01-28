415f3bdb625e0f02566e9854b578e78cc5db6059
Ravi Kumhar <ravikumharkumhar9@gmail.com>
2025-01-28 06:14:05 +0530

    This video series is 5

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,27 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
 
 const registerUser = asyncHandler( async(req, res) => {
-     
-    res.status(200).json({
+     /*Register setps
+     1 get user details from frontend
+     2 validation - not empty
+     3 check user already exists: username, email
+     4 check for images, check for avater
+     5 upload them cloudinary, avater
+     6 create user object - create entry in db
+     7 remove password and refresh token filed from response
+     8 check for user creation
+     9 return res*/
+    /*res.status(200).json({
         message: "ok"
-    })
+    })*/
+
+  const {username, email, fullName, password}  = req.body
+  console.log("email", email);
+
+  if (fullName === "") {
+    throw new ApiError(400, "fullName is required")
+  }
 })
 
 export  {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers";
+import {upload} from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+     {
+        name: "avater",
+        maxcount: 1
+     },
+     {
+        name: "coverImage",
+        maxcount: 1
+     }
+    ]),
+    registerUser);
 
 //router.route("/login").post(login);
 
